060687e4a30ec8ef3b1ed2ea7443d0455129589b
Hack Reactor Students <accounts@hackreactor.com>
2015-11-05 18:19:20 -0800

    refactored db to mongo

--- app/models/user.js
+++ app/models/user.js
@@ -1,47 +1,28 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
-// var mongoose = require('mongoose')
+var bluebird = require('bluebird');
 
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-
-var usersSchema = db.Schema ({
-    username: { type: String, unique: true, required: true },
-    password: {type: String, required: true }
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
 });
 
+var User = mongoose.model('User', userSchema);
 
-//this will listen and on every save change the password
-//middle wear - 
-usersSchema.pre('save', function(next) {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-        console.log("This is the entry....",this)
-        next();
-      });
-});
-usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.pre('save', function(next){
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
     });
-  };
-  // hashPassword: function(){
-  //   var cipher = Promise.promisify(bcrypt.hash);
-  //   return cipher(this.get('password'), null, null).bind(this)
-  //     .then(function(hash) {
-  //       this.set('password', hash);
-  //     });
-  // }
-// });
+});
 
-//now need to make a model 
-
-var User = db.model('User', usersSchema);
 module.exports = User;
